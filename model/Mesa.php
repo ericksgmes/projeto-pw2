812d174ceebeee4f2527969719e82607c2677a90
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-10-20 14:30:01 -0300

    feat: create model functions

---
+++ model/Mesa.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once(__DIR__ . "/../config/database.php");
+require_once(__DIR__ . "/../config/utils.php");
+
+class Mesa
+{
+    public static function listar() {
+        try {
+            $connection = Connection::getConnection();
+            $sql = $connection->prepare("SELECT * FROM Mesa");
+            $sql->execute();
+
+            return $sql->fetchAll();
+        } catch (Exception $e) {
+            output(500, ["msg" => $e->getMessage()]);
+        }
+    }
+
+    public static function criar($numero) {
+        try {
+            $connection = Connection::getConnection();
+            $sql = $connection->prepare("INSERT INTO Mesa(numero) VALUE (?)");
+            $sql->execute([$numero]);
+
+            return $sql->rowCount();
+        } catch (Exception $e) {
+            output(500, ["msg" => $e->getMessage()]);
+        }
+    }
+
+    public function getById($id) {
+        try {
+            $connection = Connection::getConnection();
+            $sql = $connection->prepare("SELECT * FROM Mesa WHERE id = ?");
+            $sql->execute([$id]);
+
+            return $sql->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            output(500, ["msg" => $e->getMessage()]);
+        }
+    }
+
+    public function deleteById($id){
+        try {
+            $connection = Connection::getConnection();
+            $sql = $connection->prepare("DELETE FROM Mesa WHERE id = ?");
+            $sql->execute([$id]);
+
+            return $sql->rowCount();
+
+        } catch (Exception $e) {
+            output(500, ["msg" => $e->getMessage()]);
+        }
+    }
+
+    public static function exist($id) {
+        try {
+            $connection = Connection::getConnection();
+            $sql = $connection->prepare("SELECT COUNT(*) FROM Mesa WHERE id = ?");
+            $sql->execute([$id]);
+
+            return $sql->fetchColumn();
+        } catch (Exception $e) {
+            output(500, ["msg" => $e->getMessage()]);
+        }
+    }
+
+    public static function existsByNumber($number) {
+        try {
+            $connection = Connection::getConnection();
+            $sql = $connection->prepare("SELECT id FROM Mesa WHERE numero = ?");
+            $sql->execute([$number]);
+            return (bool)$sql->fetch();
+        } catch (Exception $e) {
+            output(500, ["msg" => $e->getMessage()]);
+        }
+    }
+}
