465c1d3c4f6c64f7442cc9a196e78c49c60131e3
ericksgmes <erick.dicksan@gmail.com>
2024-11-01 17:50:12 -0300

    refactor: change database to use soft delete

--- config/init.sql
+++ config/init.sql
@@ -3,30 +3,40 @@ USE restaurante;
 
 CREATE TABLE IF NOT EXISTS Funcionario (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255),
-    username VARCHAR(255) UNIQUE,
-    senha VARCHAR(255)
+    nome VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    deletado TINYINT(1) DEFAULT 0,
+    data_deletado DATETIME NULL
     );
 
 CREATE TABLE IF NOT EXISTS Mesa (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    numero INT NOT NULL
+    numero INT NOT NULL UNIQUE,
+    deletado TINYINT(1) DEFAULT 0,
+    data_deletado DATETIME NULL
 );
 
 CREATE TABLE IF NOT EXISTS Produto (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255),
-    preco DECIMAL(10, 2)
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    preco DECIMAL(10, 2),
+    deletado TINYINT(1) DEFAULT 0,
+    data_deletado DATETIME NULL
     );
 
 CREATE TABLE IF NOT EXISTS Pagamento (
     id INT AUTO_INCREMENT PRIMARY KEY,
     metodo ENUM('DEBITO', 'CREDITO', 'PIX') NOT NULL,
     valor DECIMAL(10, 2) NOT NULL,
-    data DATETIME
+    data DATETIME,
+    id_mesa INT NOT NULL,
+    deletado TINYINT(1) DEFAULT 0,
+    data_deletado DATETIME NULL,
+    FOREIGN KEY (id_mesa) REFERENCES Mesa(id)
     );
 
-CREATE TABLE IF NOT EXISTS Atende (
+CREATE TABLE IF NOT EXISTS FuncionarioMesa (
     id_funcionario INT,
     id_mesa INT,
     PRIMARY KEY (id_funcionario, id_mesa),
@@ -35,18 +45,10 @@ CREATE TABLE IF NOT EXISTS Atende (
     );
 
 CREATE TABLE IF NOT EXISTS ProdutosMesa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_mesa INT,
     id_prod INT,
     quantidade INT,
-    PRIMARY KEY (id_mesa, id_prod),
     FOREIGN KEY (id_mesa) REFERENCES Mesa(id),
     FOREIGN KEY (id_prod) REFERENCES Produto(id)
     );
-
-CREATE TABLE IF NOT EXISTS PagamentoMesa (
-    id_mesa INT,
-    id_pag INT,
-    PRIMARY KEY (id_mesa, id_pag),
-    FOREIGN KEY (id_mesa) REFERENCES Mesa(id),
-    FOREIGN KEY (id_pag) REFERENCES Pagamento(id)
-);
