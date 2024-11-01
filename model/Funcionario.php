ec9071e900ed35335ef03d2ee28045596448716b
ericksgmes <erick.dicksan@gmail.com>
2024-11-01 17:49:37 -0300

    refactor: change model to conform to new soft delete rules.

--- model/Funcionario.php
+++ model/Funcionario.php
@@ -5,12 +5,10 @@ require_once(__DIR__ . "/../config/utils.php");
 
 class Funcionario
 {
-
-    // teste
     public static function listar() {
         try {
             $connection = Connection::getConnection();
-            $sql = $connection->prepare("SELECT * FROM Funcionario");
+            $sql = $connection->prepare("SELECT * FROM Funcionario WHERE deletado = 0");
             $sql->execute();
 
             return $sql->fetchAll();
@@ -22,10 +20,8 @@ class Funcionario
     public function login($username, $senha) {
         try {
             $connection = Connection::getConnection();
-
-            $sql = $connection->prepare("SELECT * FROM Funcionario WHERE username = ?");
+            $sql = $connection->prepare("SELECT * FROM Funcionario WHERE username = ? AND deletado = 0");
             $sql->execute([$username]);
-
             $funcionario = $sql->fetch(PDO::FETCH_ASSOC);
 
             if ($funcionario && password_verify($senha, $funcionario['senha'])) {
@@ -33,7 +29,6 @@ class Funcionario
             } else {
                 return false;
             }
-
         } catch (Exception $e) {
             output(500, ["msg" => $e->getMessage()]);
         }
@@ -46,7 +41,7 @@ class Funcionario
             $senhaHashed = password_hash($senha, PASSWORD_BCRYPT);
             $sql->execute([$nome, $username, $senhaHashed]);
 
-            return $sql->rowCount();
+            return $connection->lastInsertId();
         } catch (Exception $e) {
             output(500, ["msg" => $e->getMessage()]);
         }
@@ -67,7 +62,7 @@ class Funcionario
     public static function deleteById($id){
         try {
             $connection = Connection::getConnection();
-            $sql = $connection->prepare("DELETE FROM Funcionario WHERE id = ?");
+            $sql = $connection->prepare("UPDATE Funcionario SET deletado = 1, data_deletado = NOW() WHERE id = ?");
             $sql->execute([$id]);
 
             return $sql->rowCount();
@@ -80,9 +75,8 @@ class Funcionario
     public static function exist($id) {
         try {
             $connection = Connection::getConnection();
-            $sql = $connection->prepare("SELECT COUNT(*) FROM Funcionario WHERE id = ?");
+            $sql = $connection->prepare("SELECT COUNT(*) FROM Funcionario WHERE id = ? AND deletado = 0");
             $sql->execute([$id]);
-
             return $sql->fetchColumn();
         } catch (Exception $e) {
             output(500, ["msg" => $e->getMessage()]);
@@ -92,7 +86,7 @@ class Funcionario
     public static function existsByUsername($username) {
         try {
             $connection = Connection::getConnection();
-            $sql = $connection->prepare("SELECT id FROM Funcionario WHERE username = ?");
+            $sql = $connection->prepare("SELECT id FROM Funcionario WHERE username = ? AND deletado = 0");
             $sql->execute([$username]);
             return (bool)$sql->fetch();
         } catch (Exception $e) {
@@ -112,4 +106,27 @@ class Funcionario
         }
     }
 
+    public static function restoreById($id){
+        try {
+            $connection = Connection::getConnection();
+            $sql = $connection->prepare("UPDATE Funcionario SET deletado = 0, data_deletado = NULL WHERE id = ?");
+            $sql->execute([$id]);
+
+            return $sql->rowCount();
+
+        } catch (Exception $e) {
+            output(500, ["msg" => $e->getMessage()]);
+        }
+    }
+
+    public static function getByUsername($username) {
+        try {
+            $connection = Connection::getConnection();
+            $sql = $connection->prepare("SELECT * FROM Funcionario WHERE username = ? AND deletado = 0");
+            $sql->execute([$username]);
+            return $sql->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            output(500, ["msg" => $e->getMessage()]);
+        }
+    }
 }
