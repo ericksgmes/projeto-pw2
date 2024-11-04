a76be639d06671a0e48294a8c7a0310d6c8f3089
ericksgmes <erick.dicksan@gmail.com>
2024-11-04 01:33:38 -0300

    refactor: change index to handle requests for funcionario controller and funcionario mesa controller

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
 require_once __DIR__ . '/controller/FuncionarioController.php';
+require_once __DIR__ . '/controller/FuncionarioMesaController.php';
 require_once __DIR__ . '/config/utils.php';
 
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
@@ -13,61 +14,28 @@ $requestUri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 $data = json_decode(file_get_contents("php://input"), true);
 
-$funcionarioController = new FuncionarioController();
-
 $basePath = '/resturante-webservice';
 $relativeUri = str_replace($basePath, '', $requestUri);
 $uriSegments = explode('/', trim($relativeUri, '/'));
 
 $resource = $uriSegments[0] ?? '';
 $id = $uriSegments[1] ?? null;
+$id2 = $uriSegments[2] ?? null;
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("HTTP/1.1 200 OK");
+    exit();
+}
 
 switch ($resource) {
     case 'funcionario':
-        switch ($method) {
-            case 'GET':
-                if ($id) {
-                    $funcionarioController->listar($id);
-                } else {
-                    $funcionarioController->listar();
-                }
-                break;
+        $controller = new FuncionarioController();
+        $controller->handleRequest($method, $id, $data);
+        break;
 
-            case 'POST':
-                $funcionarioController->criar($data);
-                break;
-
-            case 'PUT':
-                if ($id) {
-                    $funcionarioController->atualizar($id, $data);
-                } else {
-                    jsonResponse(400, ["status" => "error", "message" => "ID necessário para atualização"]);
-                }
-                break;
-
-            case 'PATCH':
-                if ($id) {
-                    if (isset($data['senha'])) {
-                        $funcionarioController->atualizarSenha($id, $data['senha']);
-                    } else {
-                        jsonResponse(400, ["status" => "error", "message" => "Senha necessária para atualização"]);
-                    }
-                } else {
-                    jsonResponse(400, ["status" => "error", "message" => "ID necessário para atualização"]);
-                }
-                break;
-
-            case 'DELETE':
-                if ($id) {
-                    $funcionarioController->deletar($id);
-                } else {
-                    jsonResponse(400, ["status" => "error", "message" => "ID necessário para exclusão"]);
-                }
-                break;
-
-            default:
-                jsonResponse(405, ["status" => "error", "message" => "Método não permitido"]);
-        }
+    case 'funcionarioMesa':
+        $controller = new FuncionarioMesaController();
+        $controller->handleRequest($method, $id, $id2, $data);
         break;
 
     default:
