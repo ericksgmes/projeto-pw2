e8efbc46091f6093737b7ca7dfa730aeeca29249
ericksgmes <erick.dicksan@gmail.com>
2024-11-04 01:17:48 -0300

    fix: path treatment

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 require_once __DIR__ . '/controller/FuncionarioController.php';
 require_once __DIR__ . '/config/utils.php';
 
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 $requestUri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 $data = json_decode(file_get_contents("php://input"), true);
@@ -34,6 +40,18 @@ switch ($resource) {
             case 'PUT':
                 if ($id) {
                     $funcionarioController->atualizar($id, $data);
+                } else {
+                    jsonResponse(400, ["status" => "error", "message" => "ID necessário para atualização"]);
+                }
+                break;
+
+            case 'PATCH':
+                if ($id) {
+                    if (isset($data['senha'])) {
+                        $funcionarioController->atualizarSenha($id, $data['senha']);
+                    } else {
+                        jsonResponse(400, ["status" => "error", "message" => "Senha necessária para atualização"]);
+                    }
                 } else {
                     jsonResponse(400, ["status" => "error", "message" => "ID necessário para atualização"]);
                 }
