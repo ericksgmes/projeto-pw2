811af4493da1679c69b8fc320e2565ab793e3f74
ericksgmes <erick.dicksan@gmail.com>
2024-12-01 13:24:17 -0300

    refactor: change db structure and test data

--- config/test_data.sql
+++ config/test_data.sql
@@ -8,11 +8,11 @@ VALUES ('João Silva', 'joao', '$2y$10$tWiIWH3fEyXGMv3Oxc2a8eU0HZtHmHH2KYS4D4e7U
 
 -- Inserindo mesas
 INSERT INTO Mesa (numero)
-VALUES (1),
-       (2),
-       (3),
-       (4),
-       (5);
+VALUES ('1'),
+       ('2'),
+       ('3'),
+       ('4'),
+       ('5');
 
 -- Inserindo produtos
 INSERT INTO Produto (nome, preco)
@@ -31,22 +31,23 @@ VALUES
     ('Água com Gás', 8.00);
 
 -- Associando usuários às mesas
-INSERT INTO UsuarioMesa (id_usuario, id_mesa)
-VALUES (1, 1), -- João Silva associado à Mesa 1
-       (2, 2), -- Maria Oliveira associada à Mesa 2
-       (3, 3); -- Carlos Souza associado à Mesa 3
+INSERT INTO UsuarioMesa (id_usuario, numero_mesa)
+VALUES (1, '1'), -- João Silva associado à Mesa 1
+       (2, '2'), -- Maria Oliveira associada à Mesa 2
+       (3, '3'); -- Carlos Souza associado à Mesa 3
 
 -- Inserindo produtos pedidos em cada mesa
-INSERT INTO ProdutosMesa (id_mesa, id_prod, quantidade)
+INSERT INTO ProdutosMesa (numero_mesa, id_prod, quantidade)
 VALUES
-    (1, 1, 2), -- Mesa 1 pediu 2 Bruschettas
-    (1, 10, 1), -- Mesa 1 pediu 1 Vinho Tinto
-    (2, 4, 1), -- Mesa 2 pediu 1 Carbonara
-    (2, 3, 2), -- Mesa 2 pediu 2 Saladas Caprese
-    (3, 7, 3); -- Mesa 3 pediu 3 Tiramisù
+    ('1', 1, 2), -- Mesa 1 pediu 2 Bruschettas
+    ('1', 10, 1), -- Mesa 1 pediu 1 Vinho Tinto
+    ('2', 4, 1), -- Mesa 2 pediu 1 Carbonara
+    ('2', 3, 2), -- Mesa 2 pediu 2 Saladas Caprese
+    ('3', 7, 3); -- Mesa 3 pediu 3 Tiramisù
 
 -- Inserindo pagamentos
-INSERT INTO Pagamento (metodo, valor, data, id_mesa)
-VALUES ('CREDITO', 60.00, NOW(), 1),
-       ('DEBITO', 85.00, NOW(), 2),
-       ('PIX', 60.00, NOW(), 3);
+INSERT INTO Pagamento (metodo, valor, data, numero_mesa)
+VALUES
+    ('CREDITO', 60.00, NOW(), '1'),
+    ('DEBITO', 85.00, NOW(), '2'),
+    ('PIX', 60.00, NOW(), '3');
